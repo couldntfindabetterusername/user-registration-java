11a24b44b0c0e3a645ad07d0d3ada9fb77befc9a
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-28 18:06:15 +0530

    validating firstname

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,25 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("User registration application");
+        System.out.println("------ User registration application ------");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first name: ");
+        String firstName = scanner.nextLine();
+
+        FirstNameValidator firstNameValidator = new FirstNameValidator();
+        firstNameValidator.validate(firstName);
+
+        if (firstNameValidator.validate(firstName)) {
+            System.out.println("First name is valid");
+        } else {
+            System.out.println("First name is not valid");
+        }
+
+        scanner.close();
     }
 }
